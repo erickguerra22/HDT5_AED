1b3acfaf44ebd8fcf9e7f9fe16c45f137bd567ef
erickguerra22 <erickguerra2201@gmail.com>
2022-03-13 21:26:24 -0600

    Última actualización del programa

--- Functions.py
+++ Functions.py
@@ -1,46 +1,65 @@
-totalTime = 0
-times = []
+'''
+    Funciones utilizadas para la simulación de CPU
+    Programador:
+        - Erick Stiv Junior Guerra Muñoz
+    Fecha: 12 de marzo de 2022
+'''
+totalTime = 0 # Tiempo total que duró la simulación
+times = [] # Tiempo que duró la ejecución de cada proceso
+
+# Creacion de funciones
 def getTotalTime():
+    '''
+        Devuelve el tiempo total que duró la simulación
+    '''
     return totalTime
 
 def getTimes():
+    '''
+        Devuelve la lista del tiempo utilizado por cada proceso
+    '''
     return times
 
 def process(name, env, RAM, CPU, arrival, numIns, ramNeed, speedCPU, random):
-    yield env.timeout(arrival)
-    arrivalTime = env.now
+    '''
+        Crea el proceso con la informacion brindada
+        y luego lo ejecuta según el orden de llegada.
+    '''
+    yield env.timeout(arrival) # Simula la espera hasta la llegada del proceso
+    arrivalTime = env.now # Guarda el tiempo en el que llegó el proceso
     
+    # Se verifica si el proceso puede recibir la cantidad de memoria que necesita
     print("%s (llegada en %f) requiere %d de memoria RAM, %d disponible. Estado: NEW" %(name,arrival,ramNeed, RAM.level))
-    yield RAM.get(ramNeed)
+    yield RAM.get(ramNeed) # Espera hasta poder obtener la cantidad de memoria RAM solicitada
     print("%s ha recibido %d de memoria RAM, %d disponible. Estado: Admitted" %(name, ramNeed, RAM.level))
     
+    # Se ejecutan todas las instrucciones
     while numIns > 0:
         print("%s tiene %d instrucciones pendientes. Estado: READY" %(name, numIns))
         
         with CPU.request() as req:
-            yield req
+            yield req # Espera a que el CPU esté disponible
             
-            insMade = 0
-            numIns = numIns - speedCPU
+            insMade = speedCPU
+            numIns = numIns - speedCPU # Se restan la cantidad de instrucciones realizadas
             if numIns < 0:
                 insMade = speedCPU + numIns
-            else:
-                insMade = speedCPU
-            yield env.timeout(1)
+            yield env.timeout(1) # Simula un ciclo de reloj del CPU
             
             print("%s. El CPU ha ejecutado %d instrucciones correctamente (tiempo actual: %d). Estado: RUNNING" %(name, insMade, env.now))
-            print("Ram restante: %d" %(RAM.level))
+            print("Ram restante: %d" %(RAM.level)) # Se muestra la cantidad de memoria RAM disponible
         
         if numIns > 0:
             nextAct = random.randint(1,2)
             if nextAct == 1:
                 print("%s esta ejecutando instrucciones I/O. Estado: WAITING" %(name))
-                yield env.timeout(1)
+                yield env.timeout(1) # Simula un ciclo de reloj del CPU
     
-    yield RAM.put(ramNeed)
-    print("%s termino su ejecucion en el tiempo: %d. Nueva cantidad de memoria disponible: %d. Estado: TERMINATED" % (name, env.now, RAM.level))
+    # Al terminar de ejecutar todas las intrucciones:
+    yield RAM.put(ramNeed) # Se devuelve la cantidad de memoria RAM utilizada
+    print("%s termino su ejecucion en el tiempo: %d. Cantidad de memoria restaurada: %d. Nueva cantidad de memoria disponible: %d. Estado: TERMINATED" % (name, env.now, ramNeed, RAM.level))
     global totalTime
     global times
-    times.append(env.now - arrivalTime)
-    totalTime += env.now - arrivalTime
+    times.append(env.now - arrivalTime) # Almacena el tiempo que tomó la ejecución del proceso
+    totalTime += env.now - arrivalTime # Suma el tiempo del proceso al tiempo total de la simulación
     print("Tiempo que duro la ejecucion: %d segundos" %(env.now - arrivalTime))
--- Main.py
+++ Main.py
@@ -1,33 +1,48 @@
+'''
+    Programa que simula la ejecución de procesos en un CPU
+    Programador:
+        - Erick Stiv Junior Guerra Muñoz
+    Fecha: 12 de marzo de 2022
+'''
+
+# Importación de módulos
 import simpy
 import random
 import Functions as fc
 
-print("Bienvenido al simulador de CPU!")
+# Interfaz con el usuario
+print("Bienvenido al simulador de CPU!") # Mensaje de bienvienida
 print("Indique la siguiente informacion antes de empezar:")
-numProcess = int(input("Numero de procesos a ejecutar: "))
-intervals = int(input("Intervalo para la distribucion exponencial: "))
-ramQuantity = int(input("Capacidad de memoria RAM: "))
-numCPU = int(input("Numero de procesadores a utilizar: "))
-speedCPU = int(input("Instrucciones que el CPU es capaz de ejecutar a la vez: "))
+numProcess = int(input("Numero de procesos a ejecutar: ")) # Se solicita el número de procesos a ejecutar en la simulación
+intervals = int(input("Intervalo para la distribucion exponencial: ")) # Se solicita el número de intervalos a utilizar
+ramQuantity = int(input("Capacidad de memoria RAM: ")) # Se solicita la capacidad de memoria RAM
+numCPU = int(input("Numero de procesadores a utilizar: ")) # Se solicita el número de CPUs a utilizar
+speedCPU = int(input("Instrucciones que el CPU es capaz de ejecutar a la vez: ")) # Se solicita la velocidad de cada CPU
 
-env = simpy.Environment()
+# Inicialización de variables
+env = simpy.Environment() # Se crea el ambiente de simulación
 RAM = simpy.Container(env, init = ramQuantity ,capacity = ramQuantity)
 CPU = simpy.Resource(env, capacity = numCPU)
-seed = random.seed(10)
+random.seed(666) # Semilla que se utilizará durante toda la simulación
 promTime = 0
 standDev = 0
 
+# Creación de procesos
 for i in range(numProcess):
-    arrival = random.expovariate(1.0/intervals)
-    numIns = random.randint(1,10)
-    ramNeed = random.randint(1,10)
-    env.process(fc.process("Proceso #%s" %i, env, RAM, CPU, arrival, numIns, ramNeed, speedCPU, random))
+    arrival = random.expovariate(1.0/intervals) # Tiempo en el que llega el proceso al CPU
+    numIns = random.randint(1,10) # Número de instrucciones que realizará el proceso
+    ramNeed = random.randint(1,10) # Memoria RAM que necesita el proceso
+    env.process(fc.process("Proceso # %s" %i, env, RAM, CPU, arrival, numIns, ramNeed, speedCPU, random)) # Se crea el proceso con la información creada
     
+# Da inicio a la simulación
 input("Presione enter para iniciar la simulacion: ")
 env.run()
+
+# Se calcula e imprime el tiempo promedio que consumio cada proceso
 promTime = fc.getTotalTime()/numProcess
 print("\nTiempo promedio: %f segundos" % promTime)
 
+# Se calcula e imprime la desviación estándar de los tiempos.
 times = fc.getTimes()
 suma = 0
 for i in times:
@@ -35,3 +50,6 @@ for i in times:
  
 stanDev = (suma/(numProcess-1))**0.5
 print("\nDesviacion estandar: %f segundos" % stanDev)
+
+# Se muestra la información general que se utilizó en la simulacion
+print("Process: %d, Intervals: %d, RAM: %d, CPU: %d, Speed: %d" %(numProcess, intervals, ramQuantity, numCPU, speedCPU))
